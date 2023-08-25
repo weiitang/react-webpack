16f40ca1f51316772b2e263ef015a38cd738e526
v_weiitang <v_weiitang@tencent.com>
2023-08-25 15:02:21 +0800

    feat: webpack dev adjust

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,7 @@
 // merge，合并两个或多个webpack配置文件
 const { merge } = require('webpack-merge');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -10,13 +12,15 @@ const isProd = process.env.NODE_ENV === 'prod';
 
 // 导入公共配置文件
 const webpackConfigBase = require('./webpack.config.base');
+dotenv.config();
 
 // dev环境下相关配置
 module.exports = merge(webpackConfigBase, {
   // 指定环境
-  mode: 'development',
+  mode: 'none',
   // 输出source-map的方式，增加调试。eval是默认推荐的选择，build fast and rebuild fast！
-  devtool: 'eval',
+  // devtool: 'eval',
+  devtool: 'cheap-module-source-map',
   // 本地服务器配置
   devServer: {
     // 启动GZIP压缩
@@ -27,7 +31,7 @@ module.exports = merge(webpackConfigBase, {
     proxy: {
       '/api': {
         // 目标域名
-        target: 'http://xxxx.com:8080',
+        target: 'https://tim3dev.woa.com',
         // 允许跨域了
         changeOrigin: true,
         // 重写路径 - 根据自己的实际需要处理，不需要直接忽略该项设置即可
@@ -42,6 +46,10 @@ module.exports = merge(webpackConfigBase, {
 
   },
   plugins: [
+    // new webpack.ProvidePlugin({
+    //   process: 'process',
+    //   // Buffer: ['buffer', 'Buffer'],
+    // }),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './public/index.html')
     }),
@@ -51,7 +59,14 @@ module.exports = merge(webpackConfigBase, {
     filename: isProd ? '[name].[contenthash].css' : '[name].css',
     // 非入口的chunk文件名 - 通过import()加载异步组件中样式
     chunkFilename: isProd ? '[id].[contenthash].css' : '[id].css',
-  }),
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        ...generateEnvConsts(),
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+        'PLATFORM': '"pc"'
+      }
+    })
   ],
 
   module: {
@@ -86,3 +101,15 @@ module.exports = merge(webpackConfigBase, {
   ]
 }
 });
+
+function generateEnvConsts() {
+  const obj = {};
+  const keys = Object.keys(process.env);
+  keys.forEach((key) => {
+    if (/^[A-Z]/.test(key)) {
+      const value = process.env[key];
+      obj[key] = JSON.stringify(value);
+    }
+  });
+  return obj;
+}
